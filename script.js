cf01401c08467d82219b3746108472dbfa325c63
Luigi Vincent <luigipvincent@gmail.com>
2018-04-08 03:17:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+class Payment {
+    constructor(payment, principal, interest, totalInterest, balance) {
+        this.payment = `$${payment.toFixed(2)}`
+        this.principal = `$${principal.toFixed(2)}`
+        this.interest = `$${interest.toFixed(2)}`
+        this.totalInterest = `$${totalInterest.toFixed(2)}`
+        this.balance = `$${balance.toFixed(2)}`
+    }
+}
+
+function calculateAmortizationSchedule(balance, years, APR) {
+    let totalInterest = 0
+    let period = years * 12
+    let monthlyRate = APR / 100 / 12
+    let monthlyPayment = monthlyRate / (1 - (1 + monthlyRate) ** -period) * balance
+    let schedule = []
+
+    for (let i = 1; i <= period; i++) {
+        let interestPayment = balance * monthlyRate
+        totalInterest += interestPayment
+        let principalPayment = monthlyPayment - interestPayment
+        balance -= principalPayment
+        schedule.push(new Payment(monthlyPayment, principalPayment, interestPayment, totalInterest, balance))
+    }
+
+    return schedule
+}
