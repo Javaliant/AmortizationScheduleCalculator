2e3cd769e1cb4ee763b38239a7317380cd405c5f
Luigi Vincent <luigipvincent@gmail.com>
2018-04-09 10:17:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 class Payment {
-    constructor(payment, principal, interest, totalInterest, balance) {
+    constructor(payment, principal, interest, totalInterest, balance, equity) {
         this.payment = `$${payment.toFixed(2)}`
         this.principal = `$${principal.toFixed(2)}`
         this.interest = `$${interest.toFixed(2)}`
         this.totalInterest = `$${totalInterest.toFixed(2)}`
         this.balance = `$${balance.toFixed(2)}`
+        this.equity = (equity * 100).toFixed(2)
     }
 }
 
-function calculateAmortizationSchedule(balance, years, APR) {
+function calculateAmortizationSchedule(price, balance, years, APR, rounded = 25) {
     let totalInterest = 0
     let period = years * 12
     let monthlyRate = APR / 100 / 12
     let monthlyPayment = balance * monthlyRate / (1 - (1 + monthlyRate) ** -period)
     let schedule = []
 
-    for (let i = 1; i <= period; i++) {
+    for (let i = 1; balance > 0.01 && i <= period; i++) {
         let interestPayment = balance * monthlyRate
         totalInterest += interestPayment
-        let principalPayment = monthlyPayment - interestPayment
+        let minimumPrincipalPayment = Math.min(monthlyPayment, balance) - interestPayment
+        let principalPayment = Math.min(balance, rounded * Math.ceil(minimumPrincipalPayment / rounded))
         balance -= principalPayment
-        schedule.push(new Payment(monthlyPayment, principalPayment, interestPayment, totalInterest, balance))
+        let payment = interestPayment + principalPayment
+        schedule.push(new Payment(payment, principalPayment, interestPayment, totalInterest, balance, 1 - balance / price))
     }
 
     return schedule
